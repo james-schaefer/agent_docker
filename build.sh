f956207975a09fe5b9da9ca69f33dc3825f97651
james schaefer <jrs@slow.camp>
2026-06-25 20:41:30 -0700

    no-cache

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-docker build \
+docker build --no-cache \
   --build-arg USER_UID=$(id -u) \
   --build-arg USER_GID=$(id -g) \
   --progress=plain \
-  --no-cache \
   -t agent-dev . 2>&1 | tee build.log
